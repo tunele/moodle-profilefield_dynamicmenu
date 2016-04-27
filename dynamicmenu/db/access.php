1d96480a15f04d1154c8252c6f86225525164faa
tunele <antonello.moro1@gmail.com>
2016-04-27 11:59:33 +0200

    moodle standards

--- dynamicmenu/db/access.php
+++ dynamicmenu/db/access.php
@@ -1,10 +1,25 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 defined('MOODLE_INTERNAL') || die();
-//specific capability (requires knowing db and sql)
+// sSpecific capability (requires knowing db and sql).
 $capabilities = array(
-	'profilefield/dynamicmenu:caneditsql' => array(
-			'riskbitmask'  => RISK_CONFIG,
-			'captype'      => 'write',
-			'contextlevel' => CONTEXT_SYSTEM
-	)
+    'profilefield/dynamicmenu:caneditsql' => array(
+        'riskbitmask'  => RISK_CONFIG,
+		'captype'      => 'write',
+		'contextlevel' => CONTEXT_SYSTEM
+    )
 );
--- dynamicmenu/define.class.php
+++ dynamicmenu/define.class.php
@@ -1,10 +1,27 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * Dynamic menu profile field definition.
+ * Based on moodle menu by Shane Elliot
  *
  * @package    profilefield_dynamicmenu
  * @copyright  2016 onwards Antonello Moro {@link http://treagles.it}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
  */
 
 class profile_define_dynamicmenu extends profile_define_base {
@@ -16,19 +33,20 @@ class profile_define_dynamicmenu extends profile_define_base {
     public function define_form_specific($form) {
     	
         // Param 1 for menu type contains the options.
-        $form->addElement('textarea', 'param1', get_string('sqlquery', 'profilefield_dynamicmenu'), array('rows' => 6, 'cols' => 40));
+        $form->addElement('textarea', 'param1', get_string('sqlquery', 'profilefield_dynamicmenu'),
+            array('rows' => 6, 'cols' => 40));
         $form->setType('param1', PARAM_TEXT);
         $form->addHelpButton('param1', 'param1sqlhelp', 'profilefield_dynamicmenu');
         // Default data.
         $form->addElement('text', 'defaultdata', get_string('profiledefaultdata', 'admin'), 'size="50"');
         $form->setType('defaultdata', PARAM_TEXT);
-        
-        //let's see if the user can modify the sql
+
+        // Let's see if the user can modify the sql.
         global $USER;
         $context = context_system::instance();
-        $hascap=has_capability('profilefield/dynamicmenu:caneditsql', $context);
-        
-        if (!$hascap){
+        $hascap = has_capability('profilefield/dynamicmenu:caneditsql', $context);
+
+        if (!$hascap) {
         	$form->hardFreeze('param1');
         	$form->hardFreeze('defaultdata');
         }
