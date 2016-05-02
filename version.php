1dc544654c43d69eed3b6fa4b8107f170b4bdf17
tunele <antonello.moro1@gmail.com>
2016-05-02 16:42:12 +0200

    new version date

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016042702;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2016050201;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2015011000;        // Requires this Moodle version.
 $plugin->component = 'profilefield_dynamicmenu'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
